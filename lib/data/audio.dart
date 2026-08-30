ebaa7bd16c63f8e4635a6b41673fc284903153c2
HumanBot000 <tobias.sarosi@gmail.com>
2026-08-30 20:43:02 +0200

    implemented windows pcm player

--- lib/data/audio.dart
+++ lib/data/audio.dart
@@ -2,31 +2,21 @@ import 'package:audio_relay_x_server/config.dart';
 import 'package:audio_relay_x_server/data/server.dart';
 import 'package:flutter_pcm_sound/flutter_pcm_sound.dart';
 
-class Player {
-  static Future<void> init(
-    dynamic pcmInstance,
-    Future<void> Function(int remainingFrames) onFeed,
-  ) async {
-    await pcmInstance.init(
+class UniversalPlayer {
+  static Future<void> init() async {
+    await FlutterPcmSound.setup(
       sampleRate: defaultSampleRate,
       channelCount: defaultChannels,
     );
 
-    pcmInstance.setFeedCallback(onFeed);
-    await pcmInstance.start();
-  }
-}
+    FlutterPcmSound.setFeedCallback(_onFeed);
 
-class UniversalPlayer extends Player {
-  static Future<void> init() async {
-    await Player.init(
-      FlutterPcmSound,
-      _onFeed,
-    );
+    FlutterPcmSound.start();
   }
 
   static Future<void> _onFeed(int remainingFrames) async {
     final frame = Server.getPlaybackFrame();
+
     if (frame.isEmpty) {
       return;
     }
